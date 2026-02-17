a064ad43594b66f33e9106d15a5ac085ffb8e972
Dba17 <dbellaxomarkon@gmail.com>
2026-02-17 13:18:25 -0300

    for readability variables are placed in the same place

--- Insert.js
+++ Insert.js
@@ -2,19 +2,19 @@ import db  from "./db.js";
 
 function insertData(customerName, customerAddress, customerCompany){
     const sqlCustomer = "INSERT INTO customers(name) VALUES(?)";
+    const sqlAddress = "INSERT INTO address(address, customer_id) VALUES(?,?)";
+    const sqlCompany =  "INSERT INTO company(company,customer_id) VALUES(?,?)";
     //INSERT INTO customers TABLE
     db.query(sqlCustomer, [customerName], (err, result)=>{
         if(err) throw err;
         const customerId = result.insertId
         console.log("Customer inserted with ID:", customerId);
         //INSERT INTO address TABLE
-        const sqlAddress = "INSERT INTO address(address, customer_id) VALUES(?,?)";
         db.query(sqlAddress,[customerAddress, customerId], (err, result)=>{
             if(err) throw err;
             console.log("Address data inserted for customer ID:", customerId);
 
             //INSERT INTO company TABLE
-            const sqlCompany =  "INSERT INTO company(company,customer_id) VALUES(?,?)";
             db.query(sqlCompany, [customerCompany, customerId], (err, result)=>{
                 if (err) throw err;
                 console.log("Company inserted for customer ID", customerId)
@@ -23,4 +23,4 @@ function insertData(customerName, customerAddress, customerCompany){
     });
 }
 
-insertData("Abidar", "Debrezeyt", "Mesob")
+insertData("Eshetu", "AA", "Private")
