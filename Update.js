9406d872e4070e0c662caea7165276e3a334a6f2
Dba17 <dbellaxomarkon@gmail.com>
2026-02-18 03:23:46 -0300

    Update function

--- Update.js
+++ Update.js
@@ -1,11 +1,17 @@
 import db from "./db.js"
 
-const sql = "UPDATE customers SET name = ? WHERE customer_id=? ";
+// let make function which update values
+function updateCustomer(newName, id){
+    const sql = "UPDATE customers SET name = ? WHERE customer_id=? ";
 
-db.query(sql, ["Samuel", 4], (err, results)=>{
-    if (err){
-        console.log("Error to Update", err)
-        return;//if didn't work stop here 
-    }
-    console.log("Updated successfully row affected:", results.affectedRows)
-})
+    db.query(sql, [newName, id], (err, results)=>{   
+        if (err){
+            console.log("Error to Update", err)
+            return; //if err stop here 
+        }
+        console.log("Updated successfully row affected:", results.affectedRows)
+    })
+}
+
+updateCustomer("Felecity", 4)
+updateCustomer("Netsanet", 6)
