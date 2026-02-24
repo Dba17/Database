5d33480e20273f37d8d06034369834c50d2e4e4a
Dba17 <dbellaxomarkon@gmail.com>
2026-02-24 05:20:53 -0300

    Accepting req from insert request from frontend successfully

--- Insert.js
+++ Insert.js
@@ -1,26 +1,58 @@
 import db  from "./db.js";
+import express from "express";
+const app = express();
+//allow frontend to talk with backend
+app.use(express.urlencoded({extended: true,}));
 
-function insertData(customerName, customerAddress, customerCompany){
+app.post("/add", (req, res)=>{
+    const {name, address, company} = req.body;
+    
     const sqlCustomer = "INSERT INTO customers(name) VALUES(?)";
     const sqlAddress = "INSERT INTO address(address, customer_id) VALUES(?,?)";
     const sqlCompany =  "INSERT INTO company(company,customer_id) VALUES(?,?)";
-    //INSERT INTO customers TABLE
-    db.query(sqlCustomer, [customerName], (err, result)=>{
+
+    db.query(sqlCustomer, [name],(err, result)=>{
         if(err) throw err;
-        const customerId = result.insertId
-        console.log("Customer inserted with ID:", customerId);
-        //INSERT INTO address TABLE
-        db.query(sqlAddress,[customerAddress, customerId], (err, result)=>{
-            if(err) throw err;
-            console.log("Address data inserted for customer ID:", customerId);
+        return;
+        const customerID = result.insertId;
 
-            //INSERT INTO company TABLE
-            db.query(sqlCompany, [customerCompany, customerId], (err, result)=>{
-                if (err) throw err;
-                console.log("Company inserted for customer ID", customerId)
-            });
+        db.query(sqlAddress,[address, customerID], (err)=>{
+            if (err) throw err;
+            
         });
+        db.query(sqlCompany, [company, customerID],  (err)=>{
+                if (err) throw err;
+            });
     });
-}
+    res.end("Customer added succesfully")
+    console.log("Row Inserted successfully")
+})
 
-insertData("Eshetu", "AA", "Private")
+app.listen(3000, ()=> console.log("server running"));
+
+
+
+// function insertData(customerName, customerAddress, customerCompany){
+//     const sqlCustomer = "INSERT INTO customers(name) VALUES(?)";
+//     const sqlAddress = "INSERT INTO address(address, customer_id) VALUES(?,?)";
+//     const sqlCompany =  "INSERT INTO company(company,customer_id) VALUES(?,?)";
+//     //INSERT INTO customers TABLE
+//     db.query(sqlCustomer, [customerName], (err, result)=>{
+//         if(err) throw err;
+//         const customerId = result.insertId
+//         console.log("Customer inserted with ID:", customerId);
+//         //INSERT INTO address TABLE
+//         db.query(sqlAddress,[customerAddress, customerId], (err, result)=>{
+//             if(err) throw err;
+//             console.log("Address data inserted for customer ID:", customerId);
+
+//             //INSERT INTO company TABLE
+//             db.query(sqlCompany, [customerCompany, customerId], (err, result)=>{
+//                 if (err) throw err;
+//                 console.log("Company inserted for customer ID", customerId)
+//             });
+//         });
+//     });
+// }
+
+// insertData("Eshetu", "AA", "Private")
